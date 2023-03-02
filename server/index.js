964ae2c719c6657bd621f70028e050494a7f4014
ProKafelek12e <pro.kafelek@gmail.com>
2023-03-02 14:04:47 +0100

    endpoint pobierający dane z tabeli

--- server/index.js
+++ server/index.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const cors = require('cors')
-const mysql = require('mysql')
 
 const app = express()
 app.use(cors())
+const mysql = require('mysql')
 
 var con = mysql.createConnection({
     host: "localhost",
@@ -20,6 +20,15 @@ con.connect(function(err){
 const port = 3000
 app.get("/",function(req,res){
     res.send("pozdro")
+})
+
+app.get("/select",function(req,res){
+    const sql = "SELECT * FROM tabela1"
+        con.query(sql,function(err,result,fields){
+            if(err) console.log(err)
+            res.send(result)
+            console.log(fields)
+        })
 })
 
 
